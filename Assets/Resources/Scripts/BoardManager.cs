ec7b43671fadc405f5a27dcde55782ec5764087a
PeachTreeOath <peachtreeoath@gmail.com>
2017-07-28 23:19:44 -0600

    Resource loader

--- Assets/Resources/Scripts/BoardManager.cs
+++ Assets/Resources/Scripts/BoardManager.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public bool relationshipBoardOn = true;
+    public bool cashBoardOn = true;
+    public bool educationBoardOn = true;
+    public bool healthBoardOn = true;
+
+    private int boardCount = 10;
+
+    // Use this for initialization
+    void Start () {
+	
 	}
 	
 	// Update is called once per frame
