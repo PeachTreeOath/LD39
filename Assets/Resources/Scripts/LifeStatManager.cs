3e1234fe91a4647a64e2fc1096d2a8a44aa06f1c
Fangor <rfonger@gmail.com>
2017-07-29 01:42:47 -0600

    Adding stats

---
+++ Assets/Resources/Scripts/LifeStatManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeStatManager : MonoBehaviour {
+
+	public bool isMarried;
+    public int educationLevel;
+    public int books;
+    public int age;
+    public int maxAge;
+
+    public float GetGoldDropRate()
+    {
+        return StatConstants.instance.initialGoldDropRate * StatConstants.instance.goldDropRateScalar * educationLevel + StatConstants.instance.initialGoldDropRate;
+    }
+}
