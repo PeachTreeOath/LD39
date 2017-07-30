33d07f3bfd6c2d75a62fb43791d7bd7cf88a31cb
Dave Flanagan <dflanagan314@gmail.com>
2017-07-29 20:14:11 -0400

    Level template creator

---
+++ Assets/Resources/Scripts/dev/LevelTemplateMaker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(LevelFileIO))]
+public class LevelTemplateMaker : MonoBehaviour {
+
+    public string filename = "";
+    public string fileExt = ".lvl";
+    private string filePath = "Assets/Resources/Levels/";
+    public bool checkThisToCreateFile = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (checkThisToCreateFile) {
+            checkThisToCreateFile = false;
+            if (filename.Length == 0) {
+                Debug.LogError("You might wanna name your level");
+                return;
+            }
+            LevelFileIO io = GetComponent<LevelFileIO>();
+            if (io != null) {
+                Debug.Log("Creating level template");
+                string fn = filePath + filename + fileExt;
+                Debug.Log("file is " + fn);
+                io.createNewTemplate(fn);
+                Debug.Log("finished writing file (may take a second to appear)");
+            }
+        }
+
+		
+	}
+}
