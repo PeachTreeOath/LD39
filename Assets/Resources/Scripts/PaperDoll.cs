448572cc2d38bac91234f3e4828f060e434f7dbf
mjeffery <michael.jeffery@gmail.com>
2017-07-30 00:37:17 -0400

    and there's a creepy (paper) doll...

---
+++ Assets/Resources/Scripts/PaperDoll.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaperDoll : MonoBehaviour {
+
+    void Start() {
+        Accessorize("clothing", ResourceLoader.instance.girlHairSprites);
+        Accessorize("hair", ResourceLoader.instance.girlDressSprites);
+    }
+
+    void Accessorize(string piece, Sprite[] options) {
+        SpriteRenderer renderer = transform.Find(piece).GetComponent<SpriteRenderer>();
+        int index = Random.Range(0, options.Length);
+
+        renderer.sprite = options[index];
+    }
+
+}
