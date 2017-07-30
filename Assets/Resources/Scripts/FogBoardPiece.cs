30cfefa67ef9515a083c4fc8efa60c2f80cd49f4
PeachTreeOath <peachtreeoath@gmail.com>
2017-07-29 22:59:57 -0600

    Removed fog

--- Assets/Resources/Scripts/FogBoardPiece.cs
+++ Assets/Resources/Scripts/FogBoardPiece.cs
@@ -10,62 +10,30 @@ public class FogBoardPiece : BoardPiece
     void Start()
     {
         spr = GetComponent<SpriteRenderer>();
-        FogBoardPieceListener listener = gameObject.AddComponent<FogBoardPieceListener>();
-        listener.piece = this;
     }
 
     public void CheckDestroy()
     {
         PlayerBoardPiece player = board.GetPlayerBoardPiece();
 
-        if (player != null)
-        {
-            /* Ressurect this if we want to have true fog of war reveal rather than vision
-            if (Mathf.Abs(player.x - x) < 3)
-            {
-                Destroy(gameObject);
-            }
-            else if (Mathf.Abs(player.y - y) < 3)
-            {
-                Destroy(gameObject);
-            }
-            */
-            if (Mathf.Abs(player.x - x) < 3)
-            {
-                spr.enabled = false;
-            }
-            else
-            {
-                spr.enabled = true;
-            }
-            if (Mathf.Abs(player.y - y) < 3)
-            {
-                spr.enabled = false;
-            }
-            else
-            {
-                spr.enabled = true;
-            }
-        }
+        /* Ressurect this if we want to have true fog of war reveal rather than vision
+        if (Mathf.Abs(player.x - x) < 3)
+            Destroy(gameObject);
+        else if (Mathf.Abs(player.y - y) < 3)
+            Destroy(gameObject);
+        */
+        if (Mathf.Abs(player.x - x) < 3)
+            spr.enabled = false;
         else
-        {
-            Debug.LogError("No player found for some reason...");
-        }
-    }
+            spr.enabled = true;
 
+        if (Mathf.Abs(player.y - y) < 3)
+            spr.enabled = false;
+        else
+            spr.enabled = true;
+    }
     public override int GetContentType()
     {
         return BoardPiece.FOG;
-        return 0;
-    }
-}
-
-public class FogBoardPieceListener : TurnBehaviour
-{
-    public FogBoardPiece piece;
-
-    public override void OnAdvanceTurn()
-    {
-        piece.CheckDestroy();
     }
 }
