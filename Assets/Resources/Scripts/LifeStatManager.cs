ed25b3fa83efcf64e6b71cbeaddd6fc976605cbc
PeachTreeOath <peachtreeoath@gmail.com>
2017-07-30 03:16:42 -0600

    Added temp icons

--- Assets/Resources/Scripts/LifeStatManager.cs
+++ Assets/Resources/Scripts/LifeStatManager.cs
@@ -39,13 +39,15 @@ public class LifeStatManager : Singleton<LifeStatManager> {
         //InitiallizeFields
         maxAge = StatConstants.instance.initialMaxAge;
         age = StatConstants.instance.initialAge;
+
+        GameObject.Find("StatsPanel").gameObject.SetActive(false);
     }
 
     void Update()
     {
         //Update UI
         //Stats panel currently disabled
-        
+        /*
         relationshipStatusField.text = StatConstants.instance.RelationshipStatusString(isMarried);
         wealthField.text = "$" + PermanentStatManager.instance.wealth;
         educationLevelField.text = StatConstants.instance.EducationString(educationLevel);
@@ -53,7 +55,7 @@ public class LifeStatManager : Singleton<LifeStatManager> {
         ageField.text = "" + age;
         maxAgeField.text = "" + maxAge;
         generationField.text = "" + PermanentStatManager.instance.generation;
-        
+        */
     }
 
     public void addBooks(int amt) {
