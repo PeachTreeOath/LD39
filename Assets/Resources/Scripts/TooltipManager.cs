84a059635724cc50a5fc6a56a28c9f442c084a03
PeachTreeOath <peachtreeoath@gmail.com>
2017-07-29 21:36:11 -0600

    Adjusted tooltipmgr

--- Assets/Resources/Scripts/TooltipManager.cs
+++ Assets/Resources/Scripts/TooltipManager.cs
@@ -2,16 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TooltipManager : Singleton<TooltipManager> {
+public class TooltipManager : Singleton<TooltipManager>
+{
 
-    public Dictionary<string, string> wealthTooltips = new Dictionary<string, string>();
-    public Dictionary<string, string> healthTooltips = new Dictionary<string, string>();
+    public Dictionary<string, Tooltip> wealthTooltips = new Dictionary<string, Tooltip>();
+    public Dictionary<string, Tooltip> knowledgeTooltips = new Dictionary<string, Tooltip>();
+    public Dictionary<string, Tooltip> powerTooltips = new Dictionary<string, Tooltip>();
 
     void Start()
     {
-        //Populate dictionaries here?
-        // wealthTooltips["testKey"] = "testEntry";
-        // string retrieve = wealthTooltips["testKey"];
+        knowledgeTooltips["JUNIOR_HIGH"] = new Tooltip("Junior High", 3);
+        knowledgeTooltips["HIGH_SCHOOL"] = new Tooltip("High School", 5);
+        knowledgeTooltips["COLLEGE"] = new Tooltip("College", 3);
+        // Example: int retrieveRequirement = knowledgeTooltips["JUNIOR_HIGH"].requirement;
+    }
+}
+
+public struct Tooltip
+{
+     public string name;
+     public int requirement;
+
+    public Tooltip(string newName, int newReq)
+    {
+        name = newName;
+        requirement = newReq;
     }
 }
 
