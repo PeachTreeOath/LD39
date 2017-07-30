a6a9b6c3caa68c549f295413d4911da5109552ce
PeachTreeOath <peachtreeoath@gmail.com>
2017-07-29 21:16:50 -0600

    Tooltip manager

---
+++ Assets/Resources/Scripts/TooltipManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TooltipManager : Singleton<TooltipManager> {
+
+    public Dictionary<string, string> wealthTooltips = new Dictionary<string, string>();
+    public Dictionary<string, string> healthTooltips = new Dictionary<string, string>();
+
+    void Start()
+    {
+        //Populate dictionaries here?
+    }
+}
+
