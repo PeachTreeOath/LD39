be4e32bb7f777748faabfde5ceff31816612e537
Fangor <rfonger@gmail.com>
2017-07-30 20:13:02 -0600

    Adding transition effects

--- Assets/SceneTransitionManager.cs
+++ Assets/SceneTransitionManager.cs
@@ -7,8 +7,22 @@ public class SceneTransitionManager : Singleton<SceneTransitionManager> {
 
     public float endingTotalTime;
 
+    public float secondsToFade;
+    public float secondsToRevealHorizontalLine;
+    public float secondsToRevealVerticalLine;
+    public float secondsToPauseBeforeSceneTransition;
+
     private bool endingStarted = false;
     private float endingTime;
+    private bool horizontalWipeDone = false;
+    private bool verticalWipeDone = false;
+    private bool finalPauseDone = false;
+    private bool coroutineInProgress = false;
+
+    SpriteRenderer blackLineHorizontal;
+    SpriteRenderer blackLineVertical;
+    SpriteRenderer horizontalLineMask;
+    SpriteRenderer verticalLineMask;
 
     public void TransitionToNextLevel()
     {
@@ -27,9 +41,48 @@ public class SceneTransitionManager : Singleton<SceneTransitionManager> {
         endingTime = 0f;
         endingStarted = true;
 
+        //kicks off these coroutines
         FadeUI.instance.FadeMe();
         FadeSprite.instance.FadeMe();
+
         
+
+        
+    }
+  
+    IEnumerator DoHorizontalWipe()
+    {
+        blackLineHorizontal.enabled = true;
+        blackLineVertical.enabled = true;
+        horizontalLineMask.enabled = true;
+        verticalLineMask.enabled = true;
+        float horizontalWipeTime = 0f;
+
+        Vector3 startingScale = horizontalLineMask.transform.localScale;
+        float currentScaleX = startingScale.x;
+        Debug.Log("startingScaleX = " + currentScaleX);
+        while (currentScaleX > 0f)
+        {
+            horizontalWipeTime += Time.deltaTime;
+            Debug.Log("startingScale.x = " + startingScale.x);
+            currentScaleX = Mathf.Max(startingScale.x - (startingScale.x * horizontalWipeTime) / secondsToRevealHorizontalLine, 0);
+            Debug.Log("currentScaleX = " + currentScaleX);
+            Vector3 newScale = horizontalLineMask.transform.localScale;
+            newScale.x = currentScaleX;
+            horizontalLineMask.transform.localScale = newScale;
+            yield return null;
+        }
+        horizontalWipeDone = true;
+        coroutineInProgress = false;
+        yield return null;
+    }
+
+    void Start()
+    {
+        blackLineHorizontal = GameObject.Find("BlackLineHorizontal").GetComponent<SpriteRenderer>();
+        blackLineVertical = GameObject.Find("BlackLineVertical").GetComponent<SpriteRenderer>();
+        horizontalLineMask = GameObject.Find("HorizontalLineMask").GetComponent<SpriteRenderer>();
+        verticalLineMask = GameObject.Find("VerticalLineMask").GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -37,7 +90,23 @@ public class SceneTransitionManager : Singleton<SceneTransitionManager> {
         if (endingStarted)
         {
             endingTime += Time.deltaTime;
-            if (endingTime >= endingTotalTime)
+            if (endingTime >= secondsToFade && !horizontalWipeDone && !coroutineInProgress)
+            {
+                endingTime = 0f;
+                coroutineInProgress = true;
+                //kickoff horizontalWipe
+                StartCoroutine(DoHorizontalWipe());
+                
+            }
+            else if (endingTime >= secondsToRevealHorizontalLine && !verticalWipeDone && !coroutineInProgress)
+            {
+                //kickoff verticalWipe
+            }
+            else if (endingTime >= secondsToRevealVerticalLine && !finalPauseDone && !coroutineInProgress)
+            {
+                //kickoff finalPause
+            }
+            if (endingTime >= secondsToPauseBeforeSceneTransition && !coroutineInProgress)
             {
                 SceneManager.LoadScene("Game");
             }
