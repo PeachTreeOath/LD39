78fafabe0c6c211ac34dfeed74e9d49e815e8c27
Fangor <rfonger@gmail.com>
2017-07-30 18:37:43 -0600

    Replaced Get Married button, and scene transitions work

---
+++ Assets/SceneTransitionManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionManager : Singleton<SceneTransitionManager> {
+
+    public float endingTotalTime;
+
+    private bool endingStarted = false;
+    private float endingTime;
+
+    public void TransitionToNextLevel()
+    {
+        PermanentStatManager.instance.generation++;
+        PermanentStatManager.instance.currentLevel++;
+        playEndingEffects();
+    }
+
+    public void ReplayCurrentLevel()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    private void playEndingEffects()
+    {
+        endingTime = 0f;
+        endingStarted = true;
+
+        FadeUI.instance.FadeMe();
+        FadeSprite.instance.FadeMe();
+        
+    }
+
+    void Update()
+    {
+        if (endingStarted)
+        {
+            endingTime += Time.deltaTime;
+            if (endingTime >= endingTotalTime)
+            {
+                SceneManager.LoadScene("Game");
+            }
+        }
+    }
+
+}
